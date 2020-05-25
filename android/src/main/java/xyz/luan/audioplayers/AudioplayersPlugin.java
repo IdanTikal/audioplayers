e2bc81552d5bc824296e59e1d77a701316574d04
idan Yakobi <idan6189@gmail.com>
2020-05-25 20:42:02 +0300

    delay millis

--- android/src/main/java/xyz/luan/audioplayers/AudioplayersPlugin.java
+++ android/src/main/java/xyz/luan/audioplayers/AudioplayersPlugin.java
@@ -252,7 +252,7 @@ public class AudioplayersPlugin implements MethodCallHandler, FlutterPlugin {
             if (nonePlaying) {
                 audioplayersPlugin.stopPositionUpdates();
             } else {
-                handler.postDelayed(this, 200);
+                handler.postDelayed(this, 20);
             }
         }
     }
